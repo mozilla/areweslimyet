38af36e828c8f5cc6ccaa97cfc0c2fd6642695b0
John Schoenick <jschoenick@mozilla.com>
2012-01-10 16:25:18 -0800

    [test_months.sh] Download existing nightlies instead of compiling them

--- test_months.sh
+++ test_months.sh
@@ -24,19 +24,43 @@ for year in 2010 2011; do
         [ $day -ge 10 ] && nday=$day || nday="0$day"
         [ $month -ge 10 ] && nmonth=$month || nmonth="0$month"
         
+        # Use getnightly.py to find this build on ftp.mozilla.org
         ret=$(./getnightly.py "$year-$nmonth-$nday" 2>test_months.tmp || true)
-        if [ ! -z "$ret" ]; then
-          unset timestamp rev
-          while read line; do
-            [ -z "$timestamp" ] && timestamp="$line" || rev="$line"
-          done <<< "$ret"
-          echo ":: $month/$day/$year -- Building: $timestamp, $rev" | tee -a test_months.log
-          if ! ./slimtest_linux.sh "$rev" "$timestamp"; then
-            echo "!! Build failed, see benchtester log for $rev" | tee -a test_months.log
-          fi
-        else
+        if [ -z "$ret" ]; then
           cat test_months.tmp >> test_months.err
           echo "!! $month/$day/$year -- No build, see test_months.err" | tee -a test_months.log
+        fi
+
+        read -ar reta <<< $ret
+        timestamp=${reta[0]}
+        rev=${reta[1]}
+        url=${reta[2]}
+        echo ":: $month/$day/$year -- Building: $timestamp, $rev" | tee -a test_months.log
+        
+        # Get full commit ID from repo
+        fullrev=$(
+          cd mozilla-central
+          hg pull &>/dev/null
+          hg log -r "$rev" --template "{node}" || true
+        )
+        if [ -z "$fullrev" ]; then
+          echo "!! Couldn't lookup commit info for nightly build $rev" | tee -a test_months.log
+        fi
+        
+        # Download and extract build
+        rm -rf firefox
+        echo ":: Downloading build..."
+        if ! (curl -# "$url" | tar xj) || [ ! -d "firefox" ]; then
+          echo "!! Failed to download build from '$url'" | tee -a test_months.log
+        fi
+        
+        # Run test
+        if ! ./slimtest_linux.sh --logfile "logs/$(date +%Y%m%d_%H%M%S)_$commit.log" \
+                                 --binary firefox/firefox
+                                 --buildname "$rev" \
+                                 --buildtime "$timestamp" \
+                                 --sqlitedb slimtest.sqlite"$rev" "$timestamp"; then
+          echo "!! Build failed, see benchtester log for $rev" | tee -a test_months.log
         fi
       else
         skip=$(( $skip - 1 ))
