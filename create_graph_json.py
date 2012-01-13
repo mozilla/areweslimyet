31a448bbc8eb9808647509c44befdbc1d1548c70
John Schoenick <jschoenick@mozilla.com>
2012-01-12 16:48:10 -0800

    Initial version of the HTML & graph exporter

---
+++ create_graph_json.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Right now:
+# pull the most recent test for each build (usually only one),
+# Pull selected datapoints for that test
+
+import sys
+import os
+import sqlite3
+import json
+import time
+
+if len(sys.argv) != 3:
+  sys.stderr.write("Usage: %s <database> <outfile>\n" % sys.argv[0])
+  sys.exit(1)
+
+gDatabase = sys.argv[1]
+gOutfile = sys.argv[2]
+
+gPullData = {
+  "Fourth Iteration, All Tabs Open -- Resident Memory" : {
+    "test": "Slimtest-Talos2.1-Standalone",
+    "datapoint": "Iteration 4.TabsOpen.mem.explicit/%" ,
+    "nicename": "Fresh Start Resident Memory",
+  },
+  "Fresh Start -- Resident Memory" : {
+    "test": "Slimtest-Talos2.1-Standalone",
+    "datapoint": "Iteration 0.Start iteration.mem.explicit/%" ,
+    "nicename": "Fresh Start Resident Memory",
+  }
+}
+
+sql = sqlite3.connect(gDatabase)
+cur = sql.cursor()
+cur.row_factory = sqlite3.Row
+
+cur.execute('''SELECT `id`, `name`, `time` FROM `benchtester_builds` ORDER BY `time` ASC''')
+builds = cur.fetchall()
+
+test_names = set(map(lambda x: x['test'], gPullData.values()))
+
+data = {}
+
+for build in builds:
+  test_ids = {}
+  for n in test_names:
+    # Find ID of latest testrun for this test on this build
+    cur.execute('''SELECT * FROM `benchtester_tests` WHERE `name` = ? AND `build_id` = ? ORDER BY `time` ASC LIMIT 1''', [n, build['id']])
+    test_ids[n] = cur.fetchone()['id']
+  
+  # Pull data
+  for dname,dinfo in gPullData.items():
+    cur.execute('''SELECT * FROM `benchtester_data` WHERE `test_id` = ? AND datapoint LIKE ?''', [test_ids[dinfo['test']], dinfo['datapoint']])
+    row = cur.fetchone()
+    value = 0
+    while row:
+      value += row['value']
+      row = cur.fetchone()
+    if value:
+      if not data.has_key(dname): data[dname] = []
+      data[dname].append({ 'build': build['name'], 'time': build['time'], 'value': value})
+
+datafile = open(gOutfile, 'w')
+datafile.write("// Generated %s\n\n" % time.strftime("%b %d, %Y @ %I:%M%p %Z"))
+datafile.write("var gSlimGraphSeries = ")
+json.dump(data, datafile, indent=2)
+datafile.write(';\n')
---
+++ html/slimyet.js
@@ -0,0 +1,135 @@
+
+jQuery.new = function(e, attrs, css) {
+  var ret = jQuery(document.createElement(e));
+  if (attrs) ret.attr(attrs);
+  if (css) ret.css(css);
+  return ret;
+};
+
+//FIXME
+// Async load graph data
+
+function formatBytes(raw) {
+  function prettyFloat(aFloat) {
+    var ret = Math.round(aFloat * 100).toString();
+    if (ret == "0") return ret;
+    if (ret.length < 3)
+      ret = (ret.length < 2 ? "00" : "0") + ret;
+    
+    var clen = (ret.length - 2) % 3;
+    ret = ret.slice(0, clen) + ret.slice(clen, -2).replace(/[0-9]{3}/g, ',$&') + '.' + ret.slice(-2);
+    return clen ? ret : ret.slice(1);
+  }
+  if (raw / 1024 < 50) {
+    return prettyFloat(raw) + "B";
+  } else if (raw / Math.pow(1024, 2) < 5) {
+    return prettyFloat(raw / 1024) + "KiB";
+  } else if (raw / Math.pow(1024, 3) < 5) {
+    return prettyFloat(raw / Math.pow(1024, 2)) + "MiB";
+  } else {
+    return prettyFloat(raw / Math.pow(1024, 3)) + "GiB";
+  }
+}
+
+function addGraph(axis) {
+  if (!(axis instanceof Array))
+    axis = [ axis ];
+  
+  var seriesData = [];
+  var seriesDataPoints = [];
+  
+  for (x in axis) {
+    var data = gSlimGraphSeries[axis[x]];
+    var datapoints = [];
+    for (var i in data) {
+      datapoints.push([ data[i].time, data[i].value ]);
+    }
+    seriesData.push(data);
+    seriesDataPoints.push({ label: axis[x], data: datapoints });
+  }
+  
+  var plotbox = $.new('div', { 'id' : 'testgraph' }, { width: '1000px', height: '500px', margin: 'auto' }).appendTo(document.body);
+  var plot = $.plot(plotbox,
+    // Data
+    seriesDataPoints,
+    // Options
+    {
+      series: {
+        lines: { show: true },
+        points: { show: true }
+      },
+      grid: {
+        color: "#FFF",
+        hoverable: true,
+        clickable: true
+      },
+      xaxis: {
+        tickFormatter: function(val, axis) {
+          return new Date(val * 1000).toDateString();
+        }
+      },
+      yaxis: {
+        tickFormatter: function(val, axis) {
+          return formatBytes(val);
+        }
+      },
+      legend: {
+        backgroundColor: "#000",
+        margin: 10,
+        backgroundOpacity: 0.4
+      }
+    }
+  );
+  
+  //
+  // Tooltip
+  //
+  var tooltipHovered;
+  var mouseoverItem;
+  var tooltip = $('#tooltip')
+    .hide()
+    .appendTo(document.body)
+    .bind('mouseenter', function() {
+        tooltipHovered = true;
+      })
+    .bind('mouseleave', function() {
+        tooltipHovered = false;
+        checkHideTooltip();
+      });
+    
+  function checkHideTooltip() {
+    window.setTimeout(function () {
+      if (!mouseoverItem && !tooltipHovered && tooltip.is(':visible')) {
+        tooltip.stop().fadeTo(200, 0, function () { tooltip.hide(); });
+        plot.unhighlight();
+      }
+    }, 0);
+  }
+  $("#testgraph").bind("plothover", function (event, pos, item) {
+    if (item == mouseoverItem)
+      return;
+    mouseoverItem = item;
+    
+    if (!item) {
+      checkHideTooltip();
+      return;
+    }
+    tooltip.css({ top: item.pageY, left: item.pageX });
+    // Fill tooltip
+    tooltip.find('h2').text('Nightly');
+    var c = tooltip.find('p').empty();
+    c.append($.new('p').text(new Date(item.datapoint[0] * 1000).toDateString()));
+    c.append($.new('p').text(formatBytes(item.datapoint[1])));
+    var rev = seriesData[item.seriesIndex][item.dataIndex].build;
+    c.append($.new('a', {
+        target: '_blank',
+        href: 'https://hg.mozilla.org/mozilla-central/rev/' + rev.slice(0,12)
+      }).text(rev));
+    
+    // Show tooltip
+    plot.highlight(item.series, item.datapoint);
+    tooltip.stop().fadeTo(200, 1);
+  });
+}
+
+$(function () { addGraph(['Fourth Iteration, All Tabs Open -- Resident Memory', 'Fresh Start -- Resident Memory']); });
