17d9d68ad5bfd08c7d1b8aba83748a2aedc8c104
John Schoenick <jschoenick@mozilla.com>
2012-01-30 17:00:58 -0800

    [Endurance] Add number of iterations and per-iteration callback to doFullGC

--- mozmill_endurance_test/endurance.js
+++ mozmill_endurance_test/endurance.js
@@ -113,7 +113,7 @@ EnduranceManager.prototype = {
    * @param {function} callback
    *        Callback to call when GC/CC cycles have completed
    */
-  doFullGC: function endurance_doFullGC(callback) {
+  doFullGC: function endurance_doFullGC(callback, iterations, percyclecallback) {
     function runSoon(f)
     {
       threadMan.mainThread.dispatch({ run: f }, Ci.nsIThread.DISPATCH_NORMAL);
@@ -126,9 +126,11 @@ EnduranceManager.prototype = {
     }
     function minimizeInner()
     {
+      if (percyclecallback)
+        percyclecallback.call(null, j);
       // In order of preference: schedulePreciseShrinkingGC, schedulePreciseGC
       // garbageCollect
-      if (++j <= 10) {
+      if (++j <= iterations) {
         var schedGC = Cu.schedulePreciseShrinkingGC;
         if (!schedGC) schedGC = Cu.schedulePreciseGC;
         
