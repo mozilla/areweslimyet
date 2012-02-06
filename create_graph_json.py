6d4448a918b8e4f6e32246fef1daed8dd07ad841
John Schoenick <jschoenick@mozilla.com>
2012-02-06 13:09:25 -0800

    [create_graph_json.py] Try to generate incrementally

--- create_graph_json.py
+++ create_graph_json.py
@@ -76,46 +76,65 @@ data = {
 
 i = 0
 
+# Open the old file, if possible, to skip generating redundant data
+try:
+  last_series = gzip.open(os.path.join(gOutDir, 'series.json.gz'), 'r')
+  old_data = json.loads(last_series.read())
+  in_old_data = True
+except Exception:
+  in_old_data = False
+
 for build in builds:
   i += 1
-  print("[%u/%u] Processing build %s" % (i, len(builds), build['name']))
-  test_ids = {}
-  # Fill build_info
-  data['build_info'].append({ 'id' : build['id'], 'revision' : build['name'] })
+  if in_old_data and (
+    old_data['build_info'][i - 1]['revision'] != build['name']
+    or not os.path.exists(os.path.join(gOutDir, build['name'] + '.json.gz'))):
+    in_old_data = False
   
-  testdata = {}
-  
-  # For each test a series references, pull all data into testdata
-  for testname in test_names:
-    # Pull all data for latest run of this test on this build
-    allrows = cur.execute('''SELECT d.datapoint, d.value, t.time, t.id
-                             FROM benchtester_data d
-                             JOIN benchtester_tests t ON d.test_id = t.id
-                             LEFT JOIN benchtester_tests t2 ON t.name = t2.name AND t.build_id = t2.build_id AND t.time < t2.time
-                             WHERE t.name = ? AND t2.id IS NULL AND t.build_id = ? AND t.id IS NOT NULL
-                             ''', [testname, build['id']])
-    testdata[testname] = { 'time' : None, 'id' : None, 'allvalues' : {} };
-    for row in allrows:
-      if not testdata[testname]['time']:
-        testdata[testname]['time'] = row['time']
-        testdata[testname]['id'] = row['id']
-      testdata[testname]['allvalues'][row['datapoint']] = row['value']
+  if in_old_data:
+    print("[%u/%u] Using existing data for build %s" % (i, len(builds), build['name'])) 
+    data['build_info'].append(old_data['build_info'][i - 1])
+    for sname, sinfo in gSeriesInfo.iteritems():
+      data['series'][sname].append(old_data['series'][sname][i - 1])
+  else:
+    print("[%u/%u] Processing build %s" % (i, len(builds), build['name']))
+    test_ids = {}
+    # Fill build_info
+    data['build_info'].append({ 'id' : build['id'], 'revision' : build['name'] })
     
-  # Build all series from testdata
-  for sname, sinfo in gSeriesInfo.iteritems():
-    thisinfo = testdata[sinfo['test']]['allvalues']
+    testdata = {}
     
-    if sinfo['datapoint'][-1:] == '/':
-      value = reduce(lambda val, (k, v): val + v if k.startswith(sinfo['datapoint']) and v else val, thisinfo.iteritems(), 0)
-    else:
-      value = thisinfo[sinfo['datapoint']] if thisinfo.has_key(sinfo['datapoint']) else 0
-    data['series'][sname].append([build['time'], value])
-    
-  # Write out the test data for this build
-  testfile = gzip.open(os.path.join(gOutDir, build['name'] + '.json.gz'), 'w', 9)
-  json.dump(testdata, testfile, indent=2)
-  testfile.write('\n')
-  testfile.close()
+    # For each test a series references, pull all data into testdata
+    for testname in test_names:
+      # Pull all data for latest run of this test on this build
+      allrows = cur.execute('''SELECT d.datapoint, d.value, t.time, t.id
+                              FROM benchtester_data d
+                              JOIN benchtester_tests t ON d.test_id = t.id
+                              LEFT JOIN benchtester_tests t2 ON t.name = t2.name AND t.build_id = t2.build_id AND t.time < t2.time
+                              WHERE t.name = ? AND t2.id IS NULL AND t.build_id = ? AND t.id IS NOT NULL
+                              ''', [testname, build['id']])
+      testdata[testname] = { 'time' : None, 'id' : None, 'allvalues' : {} };
+      for row in allrows:
+        if not testdata[testname]['time']:
+          testdata[testname]['time'] = row['time']
+          testdata[testname]['id'] = row['id']
+        testdata[testname]['allvalues'][row['datapoint']] = row['value']
+      
+    # Build all series from testdata
+    for sname, sinfo in gSeriesInfo.iteritems():
+      thisinfo = testdata[sinfo['test']]['allvalues']
+      
+      if sinfo['datapoint'][-1:] == '/':
+        value = reduce(lambda val, (k, v): val + v if k.startswith(sinfo['datapoint']) and v else val, thisinfo.iteritems(), 0)
+      else:
+        value = thisinfo[sinfo['datapoint']] if thisinfo.has_key(sinfo['datapoint']) else 0
+      data['series'][sname].append([build['time'], value])
+      
+    # Write out the test data for this build
+    testfile = gzip.open(os.path.join(gOutDir, build['name'] + '.json.gz'), 'w', 9)
+    json.dump(testdata, testfile, indent=2)
+    testfile.write('\n')
+    testfile.close()
 
 data['info'] = { 'generated' : time.time() }
 
