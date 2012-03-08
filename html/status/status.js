7c88891feb32d793b5befa0b781ceb8fb85f801a
John Schoenick <jschoenick@mozilla.com>
2012-03-08 12:03:21 -0800

    [html] Push placeholder status page

--- html/status/status.js
+++ html/status/status.js
@@ -7,9 +7,26 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+"use strict";
+
+jQuery.new = function(e, attrs, css) {
+  var ret = jQuery(document.createElement(e));
+  if (attrs) ret.attr(attrs);
+  if (css) ret.css(css);
+  return ret;
+};
+
 function updateStatus(data) {
   $('#status').empty();
-  $('#status').text(data.pending.length);
+  $('#status').text('(placeholder)');
+  $('#status').append($.new('pre', null,
+                            {
+                              'text-align': 'left',
+                              'width' : '500px',
+                              'margin': 'auto',
+                              'background-color': '#433',
+                              'padding': '15px'
+                            }).text(data));
 }
 
 function statusUpdater() {
@@ -18,7 +35,7 @@ function statusUpdater() {
     success: function (data) {
       updateStatus(data);
     },
-    dataType: 'json'
+    dataType: 'text'
   });
 }
 
