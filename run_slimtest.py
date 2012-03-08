7e54bdaeaf4a4676b808949e6f3c57734e2d5f87
John Schoenick <jschoenick@mozilla.com>
2012-03-07 17:05:56 -0800

    [run_slimtest.py] Add --statusfile and --skip-existing

--- run_slimtest.py
+++ run_slimtest.py
@@ -14,6 +14,10 @@ import time
 import datetime
 import multiprocessing
 import socket
+import shlex
+import platform
+import sqlite3
+import json
 
 sys.path.append(os.path.abspath("benchtester"))
 import BuildGetter
@@ -57,12 +61,19 @@ parser.add_argument('--repo', help="For build mode, the checked out FF repo to u
 parser.add_argument('--mozconfig', help="For build mode, the mozconfig to use")
 parser.add_argument('--objdir', help="For build mode, the objdir provided mozconfig will create")
 parser.add_argument('--no-pull', action='store_true', help="For build mode, don't run a hg pull in the repo before messing with a commit")
+parser.add_argument('--status-file', help="A file to keep a json-dump of the currently running job status in. This file is mv'd into place to avoid read/write issues")
+parser.add_argument('--status-resume', action='store_true', help="Resume any jobs still present in the status file. Useful for interrupted sessions")
+parser.add_argument('--skip-existing', action='store_true', help="Check the sqlite database and skip a build if it already has complete test data")
 
 ##
 ##
 ##
 
+is_win = platform.system() == "Windows"
 logfile = None
+starttime = time.time()
+sql = None
+
 def stat(msg=""):
   msg = "%s :: %s\n" % (time.ctime(), msg)
   sys.stdout.write("[run_slimtest.py] %s" % msg)
@@ -76,11 +87,32 @@ def parse_nightly_time(string):
     raise Exception("Could not parse %s as a YYYY-MM-DD date")
   return datetime.date(int(string[0]), int(string[1]), int(string[2]))
 
+def have_test_data(build):
+  if not sql:
+    sql = sqlite3.connect('slimtest.sqlite')
+    sql.row_factory = sqlite3.Row
+
+  res = sql.execute("SELECT `id` FROM `benchtester_builds` WHERE `name` = ?", [build.get_revision()])
+  row = res.fetchone()
+  if not row: return False
+
+  res = sql.execute("SELECT `name` FROM `benchtester_tests` WHERE `build_id` = ?")
+  have_tests = set(map(lambda x: x['name'], res.fetchall()))
+  for x in AreWeSlimYetTests:
+    if not x in have_tests:
+      return False
+  return True
+
 def queue_builds(args):
   if not args['firstbuild']:
     raise Exception("--firstbuild is required")
 
   builds = []
+  def pushbuilds(buildlist):
+    if args.get('skip_existing'):
+      buildlist = filter(have_test_data, buildlist)
+    builds.extend(buildlist)
+
   mode = args['mode']
   dorange = args['lastbuild']
   # Queue builds
@@ -92,22 +124,24 @@ def queue_builds(args):
     else:
       dates = [ startdate.toordinal() ]
     for x in dates:
-      builds.append(BuildGetter.NightlyBuild(datetime.date.fromordinal(x)))
+      pushbuilds([BuildGetter.NightlyBuild(datetime.date.fromordinal(x))])
   elif mode == 'tinderbox':
     startdate = float(args['firstbuild'])
     if dorange:
       enddate = float(args['lastbuild'])
-      builds.extend(BuildGetter.get_tinderbox_builds(startdate, enddate))
+      pushbuilds(BuildGetter.get_tinderbox_builds(startdate, enddate))
     else:
-      builds.append(BuildGetter.TinderboxBuild(startdate))
+      pushbuilds([BuildGetter.TinderboxBuild(startdate)])
   elif mode == 'build':
     if not args.get('repo') or not args.get('mozconfig') or not args.get('objdir'):
       raise Exception("Build mode requires --repo, --mozconfig, and --objdir to be set")
+    
     if dorange:
-      for commit in BuildGetter.get_hg_range(args.get('repo'), args['firstbuild'], args['lastbuild'], not args.get("no_pull")):
-        builds.append(BuildGetter.CompileBuild(args.get('repo'), args.get('mozconfig'), args.get('objdir'), pull=True, commit=commit, log=None))
+      lastbuild = args['lastbuild']
     else:
-      builds.append(BuildGetter.CompileBuild(args.get('repo'), args.get('mozconfig'), args.get('objdir'), pull=True, commit=args['firstbuild'], log=None))
+      lastbuild = args['firstbuild']
+    for commit in BuildGetter.get_hg_range(args.get('repo'), args['firstbuild'], lastbuild, not args.get("no_pull")):
+      pushbuilds([BuildGetter.CompileBuild(args.get('repo'), args.get('mozconfig'), args.get('objdir'), pull=True, commit=commit, log=None)])
   else:
     raise Exception("Unknown mode %s" % mode)
   return builds
@@ -125,6 +159,41 @@ def get_queued_job(dirname):
     return bcmd
   return False
 
+# For writing builds to the status.json file, as well as resuming an interrupted
+# session by parsing that file.
+def serialize_build(build):
+  if isinstance(build, BuildGetter.CompileBuild):
+    return {
+      'type': "compile",
+      'commit': build._commit,
+      'name' : "Compile revision %s" % (build._commit,)
+      }
+  elif isinstance(build, BuildGetter.TinderboxBuild):
+    return {
+      'type' : 'tinderbox',
+      'timestamp' : build._timestamp,
+      'name' : "Tinderbox build %u" % (build._timestamp,)
+      }
+  elif isinstance(build, BuildGetter.NightlyBuild):
+    date = '%u-%u-%u' % (build._date.year, build._date.month, build._date.day)
+    return {
+      'type' : 'nightly',
+      'date' : date,
+      'name' : 'Nightly build for %s' % (date,)
+      }
+  else:
+    raise Exception("Unknown build type %s" % (build,))
+
+def deserialize_build(buildobj, args):
+  if buildobj['type'] == 'compile':
+    return BuildGetter.CompileBuild(args.get('repo'), args.get('mozconfig'), args.get('objdir'), pull=True, commit=buildobj['commit'], log=None)
+  elif buildobj['type'] == 'tinderbox':
+    return BuildGetter.TinderboxBuild(buildobj['timestamp'])
+  elif buildobj['type'] == 'nightly':
+    return BuildGetter.NightlyBuild(parse_nightly_time(buildobj['date']))
+  else:
+    raise Exception("Unkown build type %s" % buildobj['type'])
+    
 def test_build(build, buildnum, hook=None):
   mod = None
   try:
@@ -181,6 +250,21 @@ def _test_build(build, buildindex):
   
   return True
 
+def write_status(outfile, running, pending, preparing=None):
+  status = {
+            'starttime' : starttime,
+            'pending' : map(serialize_build, pending),
+            'running' : map(lambda x: serialize_build(x.build), running)
+          }
+  if preparing:
+    status['preparing'] = serialize_build(preparing)
+  sf = open(".%s" % statfile, 'w')
+  json.dump(status, sf, indent=2)
+  if is_win:
+    os.remove(statfile) # Can't do atomic renames on windows
+  os.rename(".%s" % statfile, statfile)
+  sf.close()
+
 #
 # Main
 #
@@ -189,16 +273,24 @@ if __name__ == '__main__':
   stat("Starting at %s with args \"%s\"" % (time.ctime(), sys.argv))
   args = vars(parser.parse_args())
 
+  statfile = args.get("status_file")
+  
   if args.get('log'):
     logfile = open(args.get('log'), 'a')
 
   pool = multiprocessing.Pool(processes=args['processes'])
   buildnum = 0
   running = []
+  pending = []
 
   batchmode = args.get('batch')
   if batchmode:
-    pending = []
+    if statfile and os.path.exists(statfile) and args.get('status_resume'):
+      sf = open(statfile, 'r')
+      old_status = json.load(sf)
+      sf.close()
+      for x in old_status['running'] + old_status['pending']:
+        pending.append(deserialize_build(x, args))
   else:
     pending = queue_builds(args)
 
@@ -214,25 +306,30 @@ if __name__ == '__main__':
         return False
       return True
     running = filter(clean, running)
-    
-    while batchmode and len(running) + len(pending) < args['processes']:
-      # Not enough work to fill workers, read in more jobs
+
+    # Read any pending jobs if we're in batchmode
+    while batchmode:
       bcmd = get_queued_job(batchmode)
       if not bcmd: break
-      pending.extend(queue_builds(vars(parser.parse_args(bcmd))))
+      try:
+        pending.extend(queue_builds(vars(parser.parse_args(shlex.split(bcmd)))))
+      except SystemExit, e: # Don't let argparser actually exit on fail
+        stat("Failed to parse batch file command: \"%s\"" % (bcmd,))
 
     # Prepare pending builds and put them in the run pool, but not more than
     # max + 10, as prepared builds takeup space (hundreds of queued builds would
     # fill /tmp with gigabytes of things)
-    if len(pending) and len(running) < args['processes'] + 10:
+    if len(pending) and len(running) < args['processes'] + 5:
       build = pending[0]
+      pending.remove(build)
+      if statfile:
+        write_status(statfile, running, pending, build)
       stat("Preparing build %u" % (buildnum,))
       build.prepare()
       run = pool.apply_async(test_build, [build, buildnum, args['hook']])
       run.build = build
       run.num = buildnum
       running.append(run)
-      pending.remove(build)
       buildnum += 1
    
     if len(running) + len(pending) == 0:
@@ -240,9 +337,11 @@ if __name__ == '__main__':
       break
     else:
       # Wait a little and repeat loop
-      stat("%u tasks queued, %u pending " % (len(running), len(pending)))
+      if statfile:
+        write_status(statfile, running, pending)
       time.sleep(5)
 
+  write_status(statfile, running, pending)
   stat("No more tasks exiting")
   pool.close()
   pool.join()  
