f1d91aa4ac528b5196b0c42cb2400c7a561cf095
John Schoenick <jschoenick@mozilla.com>
2012-03-09 17:30:20 -0800

    [html/status] Clear form after successful submission

--- html/status/status.js
+++ html/status/status.js
@@ -213,6 +213,7 @@ $(function () {
         if (data['result'] == 'success') {
           e.addClass('success');
           e.html("Request succeeded. It may take a few minutes for it to appear here.<br />(Caveat: If a build is in progress, new requests arn't parsed until it is complete.)");
+          $('#reqBuildType option:first').prop('selected', true);
         } else {
           e.addClass('error');
           e.text("Error: " + data['error']);
