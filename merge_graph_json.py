53599c67eb1faf7fc1e6c8628cd93954059fdc9f
John Schoenick <jschoenick@mozilla.com>
2012-03-20 15:50:10 -0700

    [merge_graph_data.py] Don't drop the last point when condensing

--- merge_graph_json.py
+++ merge_graph_json.py
@@ -59,6 +59,7 @@ def condense_data(data):
       if cday != -1: ranges.append((start, i - 1))
       cday = day
       start = i
+  ranges.append((start, i - 1))
 
   for point in ranges:
     build = {}
