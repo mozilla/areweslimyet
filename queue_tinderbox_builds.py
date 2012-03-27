9566251676d88e62c51eea84b3b2d7a2babad7e4
John Schoenick <jschoenick@mozilla.com>
2012-03-27 09:57:17 -0700

    [queue_tinderbox_builds.py] Change workings to not miss out of order builds

--- queue_tinderbox_builds.py
+++ queue_tinderbox_builds.py
@@ -7,9 +7,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# ./queue_tinderbox_builds.py <batchdir> <from>
-# Given timestamp <from>, queues all tinderbox builds on FTP *after* that time,
-# prints a single timestamp of the newest build queued. Used by cronjob to
+# ./queue_tinderbox_builds.py <batchdir> <json status file>
+# Queues all tinderbox builds on FTP not in the list in <json status file>.
+# Updates that file with list of builds considered. Used by cronjob to
 # auto-queue new tinderbox builds.
 
 import sys
@@ -23,36 +23,47 @@ def err(msg):
   stat(msg)
   sys.exit(1)
 
-if len(sys.argv) != 3 or str(int(sys.argv[2])) != sys.argv[2]:
+if len(sys.argv) != 3 or not os.path.exists(sys.argv[2]):
   err("Incorrect usage. See comments")
 
 batchdir = sys.argv[1]
-fromtime = int(sys.argv[2])
+knownbuilds = sys.argv[2]
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "benchtester")))
 import BuildGetter
 BuildGetter.output = sys.stderr
 
-builds = BuildGetter.list_tinderbox_builds(fromtime + 1)
+knownfp = file(knownbuilds, 'r')
+old_builds = json.load(knownfp)
+knownfp.close()
+
+builds = BuildGetter.list_tinderbox_builds()
 if not len(builds):
   stat("No builds to queue")
   sys.exit(0)
 
-stat("Queueing %u builds: %s" % (len(builds), builds))
-out = { 'mode' : 'tinderbox', 'prioritize' : True, 'firstbuild' : builds[0] }
-if len(builds) > 1:
-  out['lastbuild'] = builds[-1]
+stat("Comparing %u builds on tinderbox with %u known builds" % (len(builds), len(old_builds)))
 
-batchfile = os.path.join(batchdir, "tinderbox.autoqueue")
-if os.path.exists(batchfile):
-  err("Failed: file \"%s\" already exists" % (batchfile,))
+def queue_build(timestamp):
+  out = { 'mode' : 'tinderbox', 'prioritize' : True, 'firstbuild' : str(timestamp) }
+  batchfile = os.path.join(batchdir, "tinderbox-%u.autoqueue" % (timestamp,))
+  if os.path.exists(batchfile):
+    err("Failed: file \"%s\" already exists" % (batchfile,))
 
-f = open(batchfile, 'w')
-json.dump(out, f)
-f.close()
+  f = open(batchfile, 'w')
+  json.dump(out, f)
+  f.close()
 
-stat("Queued job: %s" % (out,))
+  stat("Queuing build %s" % (out,))
 
-# Only thing written to stdout. Lets us do things like
-# ./queue_tinderbox_builds.py $(cat last_build.txt) > last_build.txt
-print(builds[-1])
+i = 0
+for x in builds:
+  if x not in old_builds:
+    i += 1
+    queue_build(x)
+
+knownfp = file(knownbuilds, 'w')
+json.dump(builds, knownfp)
+knownfp.close()
+
+stat("Queued %u builds" % (i,))
