acf69e747d301062ff72b8830f400df5feced65f
John Schoenick <jschoenick@mozilla.com>
2012-04-03 14:26:28 -0700

    [FAQ/build.sh] Don't complain about existing file
    Might nuke your things.

--- faq/build.sh
+++ faq/build.sh
@@ -5,11 +5,6 @@ cd "$(dirname "$0")"
 
 target="../html/faq.htm"
 
-if [ -e "$target" ]; then
-  echo >&2 "!! Remove existing $target first"
-  exit 1
-fi
-
 cat faq.head >> "$target"
 echo "<!-- This file is generated from faq/build.sh, do not edit directly -->" >> "$target"
 markdown_py < faq.md >> "$target"
