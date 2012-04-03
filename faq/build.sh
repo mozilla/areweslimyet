00c98ecfe61b124785c56dca19626aae4a6dcfe8
John Schoenick <jschoenick@mozilla.com>
2012-04-03 15:10:34 -0700

    [FAQ/build.sh] Ensure the file is re-created

--- faq/build.sh
+++ faq/build.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 
 target="../html/faq.htm"
 
-cat faq.head >> "$target"
+cat faq.head > "$target"
 echo "<!-- This file is generated from faq/build.sh, do not edit directly -->" >> "$target"
 markdown_py < faq.md >> "$target"
 cat faq.foot >> "$target"
