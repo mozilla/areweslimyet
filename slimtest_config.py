6c8db3a1972edb8922dbf706eb09e8709768263b
John Schoenick <jschoenick@mozilla.com>
2014-01-02 14:21:49 -0600

    [slimtest_config] Add quick test config, notes

--- slimtest_config.py
+++ slimtest_config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright © 2012 Mozilla Corporation
+# Copyright © 2014 Mozilla Corporation
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -10,6 +10,21 @@
 # Tests for areweslimyet.com. Used by run_slimtest.py or slimtest_batchtester_hook.py
 
 AreWeSlimYetTests = {
+  ## A very quick test-run, for testing purposes - doesn't give very useful data
+  ## "Slimtest-TalosTP5-Quick":
+  # {
+  #   'type': "EnduranceTest",
+  #   'vars':
+  #     {
+  #       'test': [ 'mozmill_endurance_test' ],
+  #       'entities': 5,
+  #       'iterations': 1,
+  #       'delay': 0,
+  #       'perTabPause': 1,
+  #       'settleWaitTime': 3
+  #     }
+  # },
+  ## The current test used for areweslimyet.com. Takes about 90 minutes.
   "Slimtest-TalosTP5-Slow":
   {
     'type': "EnduranceTest",
@@ -34,7 +49,9 @@ AreWeSlimYetTests = {
   #       'test': [ 'mozmill_endurance_test' ],
   #       'entities': 100,
   #       'iterations': 5,
-  #       'delay': 0
+  #       'delay': 0,
+  #       'perTabPause': 0,
+  #       'settleWaitTime': 30
   #     }
   # },
 };
