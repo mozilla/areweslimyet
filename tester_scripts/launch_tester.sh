670df5c3c6c8d63a1a3c1d5e09fd4f8b31b60a3d
Eric Rahm <ericrahm@gmail.com>
2015-01-23 16:18:53 -0800

    Add a marionette version of the endurance test (#55)
    - Switch over to a MarionetteTestCase
    - Updated GC function to use about:memory's method
    - Split out checkpoint code to separate js file
      - Updated to handle multiprocess data
    - Slightly updated results format
    - Allow inserting failures without results
    - Updates EnduranceTest to use the marionette test case
      - Switches over to marionette test runner
      - Tweaks to process the new result format
      - Update jsbridge_port to marionette_port
    - Updates batchtester_hook to use marionette_port
    - Updates slimtest_config
      - Removed the old Slimtest-TalosTP5 entry
      - Added new params to quick example
      - Point to the proper test path
    - Remove mozmill_endurance_test
    - Use marionette virtualenv
    - Move the launch_tester script over to marionette
    - Adds a '--gecko_log' parameter to specify where to put the gecko log
    - Updated batchtester to specify a log location

--- tester_scripts/launch_tester.sh
+++ tester_scripts/launch_tester.sh
@@ -4,10 +4,10 @@ set -e
 
 cd "$(dirname "$0")"/..
 
-# This is a virtualenv with mozmill 1.5
-source mozmill-1.5-env/bin/activate
+# This is a virtualenv with the marionette client
+source marionette-env/bin/activate
 
-echo ":: Activated mozmill-1.5-env"
+echo ":: Activated marionette-env"
 
 clean() {
   echo >&2 ":: Cleaning house"
